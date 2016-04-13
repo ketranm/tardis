4c851eeeb4a5847834722cdf31d84241299a1202
Ke Tran <ketranmanh@gmail.com>
2016-04-13 17:15:30 +0200

    remove unused functions

--- src/util/utils.lua
+++ src/util/utils.lua
@@ -1,61 +1,20 @@
-local cjson = require 'cjson'
-
 local utils = {}
 
-
---[[
-Utility function to check that a Tensor has a specific shape.
-
-Inputs:
-- x: A Tensor object
-- dims: A list of integers
---]]
-function utils.check_dims(x, dims)
-  assert(x:dim() == #dims)
-  for i, d in ipairs(dims) do
-    local msg = 'Expected %d, got %d'
-    assert(x:size(i) == d, string.format(msg, d, x:size(i)))
-  end
-end
-
-
-function utils.get_kwarg(kwargs, name, default)
-  if kwargs == nil then kwargs = {} end
-  if kwargs[name] == nil and default == nil then
-    assert(false, string.format('"%s" expected and not given', name))
-  elseif kwargs[name] == nil then
-    return default
-  else
-    return kwargs[name]
-  end
-end
-
-
-function utils.get_size(obj)
-  local size = 0
-  for k, v in pairs(obj) do size = size + 1 end
-  return size
-end
-
-
-function utils.read_json(path)
-  local f = io.open(path, 'r')
-  local s = f:read('*all')
-  f:close()
-  return cjson.decode(s)
-end
-
-
-function utils.write_json(path, obj)
-  local s = cjson.encode(obj)
-  local f = io.open(path, 'w')
-  f:write(s)
-  f:close()
-end
-
 function utils.round(num, idp)
-  local mult = 10^(idp or 0)
-  return math.floor(num * mult + 0.5) / mult
+    local mult = 10^(idp or 0)
+    return math.floor(num * mult + 0.5) / mult
+end
+
+function utils.encodeString(input, vocab, reverse)
+    -- map a sentence to a tensor of idx
+    local xs = stringx.split(input)
+    local ids = {}
+    for i = #xs,  1, -1 do
+        local w = xs[i]
+        local idx = vocab[w] or vocab['<unk>']
+        table.insert(#ids, idx)
+    end
+    return torch.Tensor(ids):view(1, -1)
 end
 
 return utils
