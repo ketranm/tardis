5fdabffe03a28cd674ef14942694557a09455acc
Arianna Bisazza <arianna.bis@gmail.com>
2016-04-18 23:13:09 +0200

    store and print attention in nbest list

--- src/tardis/BeamSearch.lua
+++ src/tardis/BeamSearch.lua
@@ -19,12 +19,43 @@ function BeamSearch:__init(kwargs)
 
     self.K = kwargs.beamSize or 10
     self.Nw = kwargs.numTopWords or self.K
+
+    self.reverseInput = true
 end
 
 function BeamSearch:use(model)
     self.model = model
     self.model:evaluate() -- no dropout during testing
     self.dtype = model.params:type()
+end
+
+function BeamSearch:printAttention(att, numTopA, asMatrix)
+    -- att is a trgLength x srcLength soft attention matrix
+    -- topA is the number of top aligned source indices to print for each target index
+
+    --if self.reverseInput then
+    --	 topAtt = reverse(topAtt) -- HOW TO DO THIS ????
+    --end
+
+    -- create a matrix of binary values than can be easily visualized
+    local top,idTop = torch.mul(att,-1):topk(numTopA)
+    local topAtt = torch.zeros(#att):typeAs(att):scatter(2, idTop, 1)   
+
+    if asMatrix then return topAtt end
+    
+    local str = ''
+    for i = 1, topAtt:size(1) do
+        local nj = 0
+        for j = 1, topAtt:size(2) do
+            if topAtt[i][j] ~= 0 then
+                str = str .. j 
+                nj = nj + 1
+                if nj < numTopA then str = str .. '|' end
+            end
+        end
+        str = str .. ' '
+    end
+    return str
 end
 
 function BeamSearch:search(x, maxLength, ref)
@@ -40,7 +71,7 @@ function BeamSearch:search(x, maxLength, ref)
     local id2word = self.id2word
     local K, Nw = self.K, self.Nw
 
-    x = utils.encodeString(x, self.srcVocab, true)
+    x = utils.encodeString(x, self.srcVocab, self.reverseInput)
     local srcLength = x:size(2)
     local T = maxLength or utils.round(srcLength * 1.4)
 
@@ -50,25 +81,31 @@ function BeamSearch:search(x, maxLength, ref)
 
     local scores = torch.zeros(K, 1):type(self.dtype)
     local hypothesis = torch.zeros(T, K):fill(self.idx_GO):type(self.dtype)
+    -- store attention distribution a each time step:
+    local attentions = torch.zeros(K, T, srcLength):type(self.dtype)
     local completeHyps = {}
+    local completeHypsAtt = {}
     local aliveK = K
 
     -- HACK resampling attention:
     -- self.model.glimpse:setFlag(true,1)  -- this crap works only if set to 1!!
-
+    
     -- avoid using goto
     -- handle the first prediction
     local curIdx = hypothesis[1]:view(-1, 1)
     local logProb = self.model:stepDecoder(curIdx)
     local maxScores, indices = logProb:topk(K, true)  -- should be K not Nw for the first prediction only
-
+    local att = self.model.glimpse:getAttention()
+    
     hypothesis[2] = indices[1]
+    attentions[{ {},1 }] = att
     scores = maxScores[1]:view(-1, 1)
-
+   
     for i = 2, T-1 do
         local curIdx = hypothesis[i]:view(-1, 1)
         local logProb = self.model:stepDecoder(curIdx)
         local maxScores, indices = logProb:topk(Nw, true)
+        local att = self.model.glimpse:getAttention()
 
         -- previous scores
         local curScores = scores:repeatTensor(1, Nw)
@@ -90,6 +127,7 @@ function BeamSearch:search(x, maxLength, ref)
                 -- complete hypothesis
                 local cand = utils.decodeString(hypothesis[{{}, prev_k}], id2word, _ignore)
                 completeHyps[cand] = scores[prev_k][1]/i -- normalize by sentence length
+                completeHypsAtt[cand] = attentions[prev_k]:sub(1,(i-1)):clone()
             else
                 table.insert(nextIndex,  yi)
                 table.insert(expand_k, prev_k)
@@ -108,15 +146,22 @@ function BeamSearch:search(x, maxLength, ref)
         -- note: at this point nextHypothesis may contain aliveK hypotheses
         nextHypothesis[i+1]:copy(torch.Tensor(nextIndex))
         hypothesis = nextHypothesis
-        scores = torch.Tensor(nextScores):typeAs(x):view(-1, 1)
 
+        attentions[{ {},i }] = att
+        attentions = attentions:index(1, expand_k)
+
+        --print('nextHypothesis') print(nextHypothesis)   
+        --print('nextAttentions') print(attentions)        
+        
+        scores = torch.Tensor(nextScores):typeAs(x):view(-1, 1)
         self.model:indexDecoderState(expand_k)
     end
 
-
+    -- make sure we complete at least one hypothesis
     for k = 1, aliveK do
         local cand = utils.decodeString(hypothesis[{{}, k}], id2word, _ignore)
         completeHyps[cand] = scores[k][1] / (T-1)
+        completeHypsAtt[cand] = attentions[k]:sub(1,(T-2)):clone() 
     end
 
     local nBest = {}
@@ -132,11 +177,12 @@ function BeamSearch:search(x, maxLength, ref)
     for rank, hypo in ipairs(nBest) do
         -- stringx.count is fast
         local length = stringx.count(hypo, ' ') + 1
+        local align = self:printAttention(completeHypsAtt[hypo],1,false)
         if ref then
             local reward = BLEU.score(hypo, ref) * 100  -- rescale for readability
-            msg = string.format('n=%d s=%.4f l=%d b=%.4f\t%s',  rank, completeHyps[hypo], length, reward, hypo)
+            msg = string.format('n=%d s=%.4f l=%d b=%.4f\t%s\t%s',  rank, completeHyps[hypo], length, reward, hypo, align)
         else
-            msg = string.format('n=%d s=%.4f l=%d\t%s',  rank, completeHyps[hypo], length, hypo)
+            msg = string.format('n=%d s=%.4f l=%d\t%s\t%s',  rank, completeHyps[hypo], length, hypo, align)
         end
         table.insert(nBestList, msg)
     end
