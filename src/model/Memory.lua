c4ecf9f0fc0cb71f0d74f57e9ae1ae87e54ec6d5
Ke Tran <ketranmanh@gmail.com>
2016-05-04 17:42:25 +0200

    update Memory

--- src/model/Memory.lua
+++ src/model/Memory.lua
@@ -15,19 +15,18 @@ function layer:__init(memorySize)
     self.output = torch.Tensor()
 
     self.gradInput = {torch.Tensor(), torch.Tensor(), torch.Tensor()}
-
-    self.ht = torch.Tensor()
-    self.att_buffer = torch.Tensor()
     self.softmax = nn.SoftMax()
 
-    self.bufferAtt2Dims = torch.Tensor()
-    self.attProb = torch.Tensor()
+    -- temporary tensors
+    self.ht = torch.Tensor()  -- copy of h transpose
+    self.attScore3Dims = torch.Tensor()
+
     self.attProb3Dims = torch.Tensor()
-    self.att_deriv = torch.Tensor()
+    self.gradAttProb = torch.Tensor()
 end
 
 
-function layer:__slice(mem)
+function layer:__sliceOld(mem)
     --[[ Slicing memory `mem`
     In this simple implementation, we follow Tran et al
     TODO: use repeated input to get nicer gradient from lookup table
@@ -45,6 +44,25 @@ function layer:__slice(mem)
     return slice
 end
 
+function layer:__slice(mem)
+    --[[ Slicing memory `mem`
+    In this simple implementation, we follow Tran et al
+    TODO: use repeated input to get nicer gradient from lookup table
+    That is if the input indices is 1 2 3 4 5 and memorySize = 3
+    we present the input sequence 1 2 3 2 3 4 3 4 5 to the lookup table
+    nn.LookupTable will normalize gradient by the count frequency, thus gradient will be nicer
+    --]]
+
+    local T = mem:size(2)
+    assert(T % self.memorySize == 0)
+    local slice = {}
+    for t = 1, T, self.memorySize do
+        local m = mem[{{},{t, t + self.memorySize - 1}, {}}]
+        table.insert(slice, m)
+    end
+    return slice
+end
+
 function layer:updateOutput(input)
     local memIn, memOut, h = unpack(input)
     local N, T, D = h:size(1), h:size(2), h:size(3)
@@ -52,28 +70,31 @@ function layer:updateOutput(input)
 
     -- this will be a table of T 3D tensors
     -- each of them has size N, Mx, D, where Mx <= self.memorySize
-    self.sliceMemIn = self:__slice(memIn)
-    self.sliceMemOut = self:__slice(memOut)
+    self.slicedMemIn = self:__slice(memIn)
+    self.slicedMemOut = self:__slice(memOut)
 
 
-    self.ht = h:transpose(2, 3)  -- this will be (N, D, T)
+    self.ht = h:transpose(2, 3)  -- (N, D, T)
 
-    self.att_buffer:resize(N, T * Mx, 1)
+    self.attScore3Dims:resize(N, T * Mx, 1)
 
     for t = 1, T do
-        local htt = self.ht[{{}, {}, {t}}] -- this will be a (N, D, 1) tensor
-        local memIn_t = self.sliceMemIn[t]
+        local htt = self.ht[{{}, {}, {t}}]  -- (N, D, 1) tensor
+        local memIn_t = self.slicedMemIn[t]
 
-        self.att_buffer[{{},{(t-1) * Mx + 1, t * Mx},{}}]:bmm(memIn_t, htt)
+        self.attScore3Dims[{{}, {(t-1) * Mx + 1, t * Mx}, {}}]:bmm(memIn_t, htt)
     end
 
-    self.bufferAtt2Dims = self.att_buffer:view(-1, Mx)  -- this will be (N * T, Mx)
-    self.attProb = self.softmax(self.bufferAtt2Dims)
-    self.attProb3Dims = self.attProb:view(N, T, Mx)
+    self.attScore2Dims = self.attScore3Dims:view(-1, Mx)  -- (N * T, Mx)
+
+    local attProb2Dims = self.softmax(self.attScore2Dims)
+
+    self.attProb3Dims = attProb2Dims:view(N, T, Mx)
 
     self.output:resize(N, T, D)
+
     for t = 1, T do
-        self.output[{{},{t},{}}]:bmm(self.attProb3Dims[{{},{t},{}}], self.sliceMemOut[t])
+        self.output[{{},{t},{}}]:bmm(self.attProb3Dims[{{},{t},{}}], self.slicedMemOut[t])
     end
 
     return self.output
@@ -88,35 +109,37 @@ function layer:backward(input, gradOutput)
     local gradMemIn, gradMemOut, grad_h = unpack(self.gradInput)
     gradMemIn:resizeAs(memIn):zero()
     gradMemOut:resizeAs(memOut):zero()
-    grad_h:resizeAs(self.ht):zero()
+    grad_h:resizeAs(h):zero()
 
     -- each element of the slice is a (N, Mx, D) tensor
-    local sliceGradMemIn = self:__slice(gradMemIn)
-    local sliceGradMemOut = self:__slice(gradMemOut)
+    local slicedGradMemIn = self:__slice(gradMemIn)
+    local slicedGradMemOut = self:__slice(gradMemOut)
 
 
-    self.att_deriv:resize(N, T, Mx)  -- same size as attProb3Dims
+    self.gradAttProb:resize(N, T, Mx)  -- same size as attProb3Dims
     self.attProb3Dims = self.attProb3Dims:transpose(2, 3)  -- (N, Mx, T)
+
     for t = 1, T do
-        local att_t = self.attProb3Dims[{{}, {}, {t}}]  -- (N, Mx, 1)
+        local attn_t = self.attProb3Dims[{{}, {}, {t}}]  -- (N, Mx, 1)
         local dout_t = gradOutput[{{}, {t}, {}}]  -- (N, 1, D)
-        sliceGradMemOut[t]:bmm(att_t, dout_t)  -- (N, Mx, D)
-        self.att_deriv[{{}, {t}, {}}]:bmm(dout_t, self.sliceMemOut[t]:transpose(2, 3))  -- (N, 1, Mx)
+        slicedGradMemOut[t]:bmm(attn_t, dout_t)  -- (N, Mx, D)
+        self.gradAttProb[{{}, {t}, {}}]:bmm(dout_t, self.slicedMemOut[t]:transpose(2, 3))  -- (N, 1, Mx)
     end
 
-    self.att_deriv = self.att_deriv:view(-1, Mx)  -- (N * T, Mx)
-    local gradSoftMax = self.softmax:backward(self.bufferAtt2Dims, self.att_deriv)
-    gradSoftMax = gradSoftMax:view(N, -1 , 1)  -- (N, T * Mx, 1)
-    -- double check this part
+    self.gradAttProb = self.gradAttProb:view(-1, Mx)  -- (N * T, Mx)
+
+    local gradAttScore = self.softmax:backward(self.attScore2Dims, self.gradAttProb)
+
+    gradAttScore = gradAttScore:view(N, -1 , 1)  -- (N, T * Mx, 1)
+
     for t = 1, T do
         local ht = h[{{}, {t}, {}}] -- (N, 1, D)
         -- get out grad of the unnormalized attention
-        local gradAtt = gradSoftMax[{{},{(t-1) * Mx + 1, t * Mx}, {}}] -- (N, Mx, 1)
-        grad_h[{{}, {}, {t}}]:bmm(self.sliceMemIn[t]:transpose(2, 3), gradAtt)
-        sliceGradMemIn[t]:bmm(gradAtt, ht)
+        local grad_attn_t = gradAttScore[{{},{(t-1) * Mx + 1, t * Mx}, {}}]  -- (N, Mx, 1)
+        grad_h[{{}, {t}, {}}]:bmm(grad_attn_t:transpose(2, 3), self.slicedMemIn[t])
+        slicedGradMemIn[t]:bmm(grad_attn_t, ht)
     end
 
-    self.gradInput[3] = grad_h:transpose(2, 3)
     return self.gradInput
 end
 
@@ -124,6 +147,10 @@ end
 function layer:clearState()
     nn.utils.clear(self, {
         'output',
-        'gradInput'
+        'gradInput',
+        'ht',
+        'attScore3Dims',
+        'attProb3Dims',
+        'gradAttProb'
     })
 end
