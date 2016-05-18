e8ab01144e4e3775a1572cad75afbb8c3069e51a
Ke Tran <ketranmanh@gmail.com>
2016-05-18 02:09:46 +0200

    factorize code

--- src/model/NMTA.lua
+++ src/model/NMTA.lua
@@ -60,7 +60,6 @@ function NMT:__init(config)
     -- use buffer to store all the information needed for forward/backward
     self.buffers = {}
     self.output = torch.LongTensor()
-    self.prob = torch.Tensor()
 end
 
 function NMT:selectState()
@@ -89,7 +88,7 @@ function NMT:forward(input, target)
 
 end
 
-function NMT:backward(input, target)
+function NMT:backward(input, target, gradOutput)
     -- zero grad manually here
     self.gradParams:zero()
 
@@ -102,10 +101,13 @@ function NMT:backward(input, target)
     local logProb = buffers.logProb
 
     -- all good. Ready to back-prop
+    local gradLoss = gradOutput
+    -- by default, we use Cross-Entropy loss
+    if not gradLoss then
+        gradLoss = self.criterion:backward(logProb, target)
+        gradLoss:div(self.numSamples)
+    end
 
-    local gradLoss = self.criterion:backward(logProb, target)
-    -- self.sum already count in the forward pass
-    gradLoss:div(self.numSamples)
     local gradLayer = self.layer:backward({context, outputDecoder}, gradLoss)
     local gradDecoder = gradLayer[2] -- grad to decoder
     local gradGlimpse =
@@ -198,17 +200,22 @@ function NMT:stepDecoder(x)
 end
 
 -- REINFORCE training Neural Machine Translation
-function NMT:sample(nsteps)
+function NMT:sample(nsteps, k)
     --[[ Sample `nsteps` from the model
     Assume that we already run the encoder and started reading in <s> symbol,
     so the buffers must contain log probability of the next words
 
     Parameters:
     - `nsteps` : integer, number of time steps
-
+    - `k` : sample from top k
     Returns:
     - `output` : 2D tensor of sampled words
     --]]
+
+    if not self.prob then
+        self.prob = torch.Tensor():typeAs(self.params)
+    end
+
     local buffers = self.buffers
     local outputEncoder, prevState = buffers.outputDecoder, buffers.prevState
     self.decoder:initState(prevState)
@@ -222,218 +229,18 @@ function NMT:sample(nsteps)
         self.prob:resizeAs(logProb)
         self.prob:copy(logProb)
         self.prob:exp()
-        self.prob.multinomial(self.output[{{}, {i}}], self.prob, 1)
+        if k then
+            local prob_k, idx = self.prob:topk(k, true)
+            prob_k:cdiv(prob_k:sum(2):repeatTensor(1, k)) -- renormalized
+            local sample = torch.multinomial(prob_k, 1)
+            self.output[{{}, {i}}] = idx:gather(2, sample)
+        else
+            self.prob.multinomial(self.output[{{}, {i}}], self.prob, 1)
+        end
         logProb = self:stepDecoder(self.output[{{},{i}}])
     end
+
     return self.output
-end
-
-function NMT:initMixer(config)
-    print('init mixer')
-
-    self.eos_idx = config.eos_idx
-    self.pad_idx = config.pad_idx
-    self.unk_idx = config.unk_idx
-
-    local dtype = self.params:type()
-
-    -- setup xent criterion
-    self.wxent = 0.5  -- hard-coded for now
-    local weights = torch.ones(config.trgVocabSize)
-    weights[config.pad_idx] = 0
-    self.criterion_xe = nn.ClassNLLCriterionWeighted(
-        self.wxent, weights, false)
-    self.criterion_xe:type(dtype)
-    -- set up reinforce criterion
-    local reward_func =
-        RewardFactory(config.trgVocabSize,
-                      self.eos_idx,
-                      self.unk_idx,
-                      self.pad_idx)
-    self.criterion_rf = RFCriterion(reward_func,
-                        self.eos_idx, self.pad_idx, 1)
-    self.criterion_rf:setWeight(1 - self.wxent)
-    self.criterion_rf:type(dtype)
-    self.criterion_rf:training_mode()
-
-    -- should we setup the cumulative reward predictor here?
-    -- lets do it first, think later
-    -- this is different from the original MIXER code
-    -- we create only one instance of the regressor
-    -- and we use it though different time steps
-    -- I think it does not make much different
-    local crp = nn.Linear(config.hiddenSize, 1)
-    crp.bias:fill(0.01)
-    crp.weight:fill(0)
-    crp:type(dtype)
-    self.crp = crp
-    self.param_crp, self.grad_param_crp = crp:getParameters()
-    -- configuration and state for optim
-    -- it is more stable with adaptive learning rate method
-    self.crp_cfg = {}
-    self.crp_sta = {}
-    -- buffers
-    self.drf = torch.Tensor():type(dtype)
-    self.vocab_size = config.trgVocabSize
-end
-
-function NMT:save_crp(fname)
-    -- this is useful for long training
-    self.grad_param_crp:zero()
-    local crp = {self.crp, self.crp_cfg, self.crp_sta}
-    torch.save(fname, crp)
-end
-
-function NMT:load_crp(fname)
-    local crp = torch.load(fname)
-    self.crp, self.crp_cfg, self.crp_sta = unpack(crp)
-end
-
-function NMT:padify(pred)
-    -- when previous token was eos or pad
-    -- pad is produced deterministically
-    local mbsz, length = pred:size(1), pred:size(2)
-    local mask1 = torch.Tensor(mbsz):typeAs(pred)
-    local mask2 = torch.Tensor(mbsz):typeAs(pred)
-    for tt = 2, length do
-        torch.eq(mask1, pred[{{},tt - 1}], self.eos_idx)
-        torch.eq(mask2, pred[{{},tt - 1}], self.pad_idx)
-        mask1:add(mask2)
- 
-        torch.ne(mask2, mask1, 1) -- negate
-        mask2:cmul(pred[{{}, tt}])
-        mask1:mul(self.pad_idx)
-        mask1:add(mask2)
-        pred[{{}, tt}]:copy(mask1)
-    end
-    return pred
-end
-
-function NMT:trainMixer(input, target, skips, learningRate)
-    --[[ train MIXER on one mini-batch
-    Parameters:
-    - `input` : table of (src, target_history)
-    - `target` : next_target
-    - `skips` : integer, length of reference policy, ie. xent loss
-
-    --]]
-    -- probably we should not put this code here
-    -- TODO: avoid passing data twice
-    local src, trg_inpt = unpack(input)
-    local length = trg_inpt:size(2)
-    local mbsz = src:size(1)
-    local length_xe = skips - 1
-    local length_rf = length - skips + 1
-
-    -- be careful with the indices
-    self:stepEncoder(input[1])
-    if length_xe > 0 then
-        local x = trg_inpt:narrow(2, 1, length_xe) --:contiguous()
-        self:stepDecoder(x)
-    end
-    -- now we start with one step
-    local x = trg_inpt[{{}, {length_xe + 1}}] --:contiguous()
-    self:stepDecoder(x)
-
-    --[[ runing example
-    skips = 4, length = 6
-    length_rf = 6 - 4 + 1 = 3
-    inp : - - - x  s1 s2
-    out : - - - s1 s2 s3
-    --]]
-
-    local sampled_w = self:sample(length_rf)
-    self:padify(sampled_w) -- padding when eos is found
-
-    x = trg_inpt:clone() -- do we need to clone?
-    x[{{}, {length_xe + 2, -1}}] = sampled_w[{{}, {1, -2}}]
-
-    local y = target:clone()
-    y[{{}, {length_xe + 1, -1}}] =  sampled_w
-
-    -- roll in the RNNs
-    self.buffers.prevState = self.encoder:lastState()
-    local logProb = self:stepDecoder(x)
-
-    -- compute reinforce loss and gradient
-    self.criterion_rf:setSkips(skips)
-
-    -- using a baseline to reduce variance
-    local state = self:selectState()
-    local pred_crw = self.crp:forward(state)
-    local baseline = pred_crw:viewAs(x)
-    -- when pad is found, the baseline should predict zero
-    -- otherwise, shit happens
-    baseline:cmul(x:ne(self.pad_idx))
-
-    local reward = self.criterion_rf:forward({y, baseline}, target)
-    local grad_rf  = self.criterion_rf:backward({y, baseline}, target)
-    local grad_crp = grad_rf[2]
-
-    -- use Adadelta to optimize the baseline regressor
-    local feval = function(x)
-        self.grad_param_crp:zero()
-        local crp_err = grad_crp:norm()
-        local num_generated_words = mbsz * length_rf
-        crp_err = crp_err^2 / num_generated_words
-
-        self.crp:backward(state, grad_crp:view(-1, 1))
-        return crp_err, self.grad_param_crp
-    end
-
-    local _, fx = optim.adadelta(feval, self.param_crp, self.crp_cfg, self.crp_sta)
-    local crp_err = fx[1]
-
-    -- Overwrite target as we do not need it anymore
-    -- use padding to ignore sampled words
-    target[{{}, {length_xe + 1, -1}}]:fill(self.pad_idx)
-    target = target:view(-1)
-    self.tot:ne(target, self.pad_idx)
-    self.numSamples = self.tot:sum()
-    local nll = self.criterion_xe:forward(logProb, target)
-    nll = nll / self.numSamples
-
-    -- Compute the gradient of MIXER
-    -- (1) take gradient of XENT
-    local gradLoss = self.criterion_xe:backward(logProb, target)
-    -- (2) normalize it
-    gradLoss:div(self.numSamples)
-    -- (3) take gradient of REINFORCE
-    self.drf:resize(mbsz, length, self.vocab_size):zero()
-    self.drf:scatter(3, y:view(mbsz, -1, 1), grad_rf[1]:view(mbsz, -1, 1))
-    -- (4) add it to gradient of XENT
-    gradLoss:add(self.drf:view(-1, self.vocab_size))
-
-
-    --------------   ok! ready to bprop  ------------------
-    self.gradParams:zero()
-    local buffers = self.buffers
-    local outputEncoder = buffers.outputEncoder
-    local outputDecoder = buffers.outputDecoder
-    local context = buffers.context
-    local logProb = buffers.logProb
-
-    local gradLayer = self.layer:backward({context, outputDecoder}, gradLoss)
-    local gradDecoder = gradLayer[2] -- grad to decoder
-    local gradGlimpse =
-        self.glimpse:backward({outputEncoder, outputDecoder}, gradLayer[1])
-
-    gradDecoder:add(gradGlimpse[2]) -- accummulate gradient in-place
-
-    self.decoder:backward(input[2], gradDecoder)
-    -- initialize gradient from decoder
-    self.encoder:setGradState(self.decoder:getGradState())
-    -- backward to encoder
-    local gradEncoder = gradGlimpse[1]
-    self.encoder:backward(input[1], gradEncoder)
-    self:update(learningRate)
-
-    -- reward is negative (we are minimizing)
-    -- the key to hapiness is to have low expectations
-    -- so we return the true numbers
-    return {nll / self.wxent,
-            -reward / (1 - self.wxent),
-            crp_err}
 end
 
 function NMT:indexDecoderState(index)
