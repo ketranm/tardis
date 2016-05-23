e106c5a28bafee2454c36d0251648469aacf37a6
Ke Tran <ketranmanh@gmail.com>
2016-05-23 22:04:52 +0200

    delete

--- src/test/seq2seq.lua
+++ src/test/seq2seq.lua
@@ -1,15 +1,14 @@
 require 'torch'
 require 'nn'
-require 'model.NMTA'
+require 'seq2seq.NMTA'
 
-local kwargs = {srcVocabSize = 100,
+local config = {srcVocabSize = 100,
             trgVocabSize = 100,
             embeddingSize = 10, 
-            hiddenSize = 10,
-            numLayer = 4, dropout = 0,
-            batch_norm = false}
+            hiddenSize = 10, pad_idx = 1,
+            numLayers = 4, dropout = 0}
 
-local model = nn.NMT(kwargs)
+local model = nn.NMT(config)
 local batchSize, tx, ty = 5, 7, 11
 local x = torch.range(1, batchSize * tx):reshape(batchSize, tx)
 local y = torch.range(1, batchSize * ty):reshape(batchSize, ty)
@@ -19,8 +18,8 @@ next_y:sub(1,-1,-1,-1):fill(100)
 
 local nll = 0
 for i = 1, 100 do
-    nll = model:forward({x,y}, next_y:view(-1))
-    model:backward({x,y}, next_y:view(-1))
+    nll = model:forward({x,y}, next_y)
+    model:backward({x,y}, next_y)
     model:update(1)
     print(i, nll)
 end
