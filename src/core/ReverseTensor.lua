eb6dc206b1502c3d5697fb63cf72794de695ae66
Ke Tran <ketranmanh@gmail.com>
2016-06-16 17:08:04 -0700

    fixed backward pass

--- src/core/ReverseTensor.lua
+++ src/core/ReverseTensor.lua
@@ -6,29 +6,27 @@ local ReverseTensor, parent = torch.class('nn.ReverseTensor', 'nn.Module')
 function ReverseTensor:__init(dim, bprop)
     parent.__init(self)
     self.dim = dim or 1
-    self.rev_index = torch.LongTensor()
     self.max_seq = 1000
-    self.rev_index = torch.range(self.max_seq, 1, -1)
+    self.rev_index = torch.range(self.max_seq, 1, -1):long()
     if bprop ~= nil then
         self.bprop = bprop
     else
         self.bprop = true
     end
+    self.idx = nil
 end
 
 function ReverseTensor:updateOutput(input)
     assert(input:dim() >= self.dim, 'invalid input!')
-    local D = input:size(self.dim)
-    --self.rev_index:range(input:size(self.dim), 1, -1)
-    self.output = input:index(self.dim, self.rev_index:narrow(1, self.max_seq - D + 1, D))
+    local T = input:size(self.dim)
+    self.idx = self.rev_index:narrow(1, self.max_seq - T + 1, T)
+    self.output = input:index(self.dim, self.idx)
     return self.output
 end
 
 function ReverseTensor:updateGradInput(input, gradOutput)
     if self.bprop then
-        print('check--->', input:size())
-        local D = input:size(self.dim)
-        self.gradInput = gradOutput:index(self.dim, self.rev_index:narrow(1, self.max_seq - D + 1, D))
+        self.gradInput = gradOutput:index(self.dim, self.idx)
     else
         self.gradInput:resize(0)
     end
