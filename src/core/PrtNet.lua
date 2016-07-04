25580a937984608867958256a5e8f2668c8dff3a
Ke Tran <ketranmanh@gmail.com>
2016-07-03 19:54:13 -0700

    use more samples for REINFORCE

--- src/core/PrtNet.lua
+++ src/core/PrtNet.lua
@@ -1,16 +1,17 @@
 local THNN = require 'nn.THNN'
 -- Pointer Network trained with REINFORCE
-
+-- experimental setting
 local PrtNet, parent = torch.class('nn.PrtNet', 'nn.Module')
 require 'core.SqueezeDiag'
 
-function PrtNet:__init(lambda)
+function PrtNet:__init(lambda, nsamples)
     self.lsm = nn.LogSoftMax()
     self.xt = nil
     self.scores = torch.Tensor()
     self.lsm = nn.LogSoftMax()
     self._sample = torch.LongTensor()
     self.sample = torch.LongTensor()
+    self.nsamples = nsamples or 3 -- use 5 samples by default
     self.lambda = lambda or 0.01 -- reinforce weight
     self.sqd = nn.SqueezeDiag(1000)
     self.b = 0 -- baseline
@@ -54,8 +55,13 @@ function PrtNet:forward(input)
     self.logp = self.lsm(my_scores)
     self.prob = torch.exp(self.logp)
     -- sample this shit
-    self.prob.multinomial(self._sample, self.prob, 1)
-    self.reward = self.logp:gather(2, self._sample)
+    self.prob.multinomial(self._sample, self.prob, self.nsamples, true)
+    -- handling nsamples
+
+    self.logp = self.logp:repeatTensor(self.nsamples, 1) --  create nsamples copy
+    self._sample = self._sample:t():contiguous()
+    -- compute reward, we need to scale xent loss with the reward later
+    self.reward = self.logp:gather(2, self._sample:view(-1, 1))
     self.output = self.xent:forward(self.logp, self._sample:view(-1))
     return self.output
 end
@@ -88,7 +94,15 @@ function PrtNet:backward(input, gradOutput)
     self.reward:add(-1 * self.b):mul(self.lambda)
     local _reward = self.reward:repeatTensor(1, self.T - 1)
     dxent:cmul(_reward)
-    local dsqd = self.lsm:backward(self.buffer, dxent)
+    -- collapse gradient
+    local df = dxent:split(self.N * self.T)
+    assert(#df == self.nsamples)
+    local dfx = df[1]
+    for i = 2, self.nsamples do
+        dfx:add(df[i])
+    end
+    -- done collapsing gradient of samples
+    local dsqd = self.lsm:backward(self.buffer, dfx)
     local dbuff = self.sqd:backward(self.scores, dsqd:view(self.N, self.T, -1))
 
     local dx, dy = self.gradInput[1], self.gradInput[2]
